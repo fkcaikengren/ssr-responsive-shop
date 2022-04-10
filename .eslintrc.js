86c1050a0ccd85096de44322d4f6f56bc84d9d78
fkcaikengren <fkcaikengren@163com>
2022-04-10 19:18:05 +0800

    fix: stylelint

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,8 +18,9 @@ module.exports = {
     'airbnb',
     // Use eslint-plugin-promise as recommended
     'plugin:promise/recommended',
-    // Use eslint-config-prettier & eslint-plugin-prettier, it can disable all rules which conflict with prettier.
-    'plugin:prettier/recommended',
+    // Use eslint-config-prettier. 只是禁止里那些不符合prettier风格的eslint rules.
+    'prettier', // 这不会调用prettier插件进行格式化，除非你手动安装开启了vscode prettier插件
+    // 'plugin:prettier/recommended', //use eslint-config-prettier & eslint-plugin-prettier (在eslint层面开启prettier插件，会在保存时格式化，在一些方面容易和eslint冲突)
   ],
   settings: {
     'import/resolver': {
@@ -30,13 +31,13 @@ module.exports = {
     },
   },
   // declare plugins then you can use the rules of them.
-  plugins: ['import', 'promise', 'prettier'],
+  plugins: ['import', 'promise'],
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     'import/prefer-default-export': 'off',
     'import/no-unresolved': ['error', { commonjs: true }],
     'promise/always-return': 'off',
-    'prettier/prettier': 'off', // use prettier to format
+    semi: ['warn', 'always'],
     'max-len': ['warn', { code: 100, comments: 150 }],
     indent: 'off',
     'implicit-arrow-linebreak': ['error', 'beside'],
